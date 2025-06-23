1910cacb0f25b957af75fd533885530576100ac0
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-23 16:21:08 +0530

    Operators in cpp

---
+++ Operator.cpp
@@ -0,0 +1,152 @@
+//increment and decrement
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10;
+
+//     cout<<"a++ is "<<a++<<endl;
+//     cout<<"++a is "<<++a<<endl;
+
+//     a = 15;
+//     cout<<"a-- is "<<a--<<endl;
+//     cout<<"--a is "<<--a<<endl;
+//     return 0;
+// }
+
+//all arithmetic operator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10,b = 20;
+
+//     cout<<"the addition is "<<a+b<<endl;
+//     cout<<"the subtarction is "<<a-b<<endl;
+//     cout<<"the multiplication is "<<a*b<<endl;
+//     cout<<"the division is "<<a/b<<endl;
+//     cout<<"the modulus is "<<a%b<<endl;
+    
+// }
+
+
+//logical operator
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 6, b = 8;
+
+//     cout<<(a && b)<<endl;
+//     cout<< (a || b)<<endl;
+// }
+
+
+//Assignment Operator
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 20;
+//     cout<<"value of a is "<<a<<endl;
+
+//     cout<<"value of a is "<<(a+=20)<<endl;
+
+//     cout<<"value of a is "<<(a-=20)<<endl;
+
+//     cout<<"value of a is "<<(a*=20)<<endl;
+
+//     cout<<"value of a is "<<(a/=20)<<endl;
+
+//     cout<<"value of a is "<<(a%=20)<<endl;
+// }
+
+
+//comparision operator
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a = 10, b = 20;
+
+//     cout<<(a<b)<<endl;
+//     cout<<(a>b)<<endl;
+//     cout<<(a>=b)<<endl;
+//     cout<<(a<=b)<<endl;
+//     cout<<(a==b)<<endl;
+//     cout<<(a!=b)<<endl;
+// }
+
+
+//operator precedence and associativity
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10 , b= 20, c = 30;
+
+//     cout<<a+b*c<<endl;
+//     cout<<a-b*c<<endl;
+//     cout<<a*b+c<<endl;
+//     cout<<a/b*c<<endl;
+//     cout<<a*b/c<<endl;
+// }
+
+
+//BITWISE OPERATOR
+//Bitwise and(&) operator
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a = 5,b = 3;
+//     cout<<(a & b)<<endl;
+//     return 0;
+// }
+
+// bitwise or(|) operator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 5, b = 3;
+//     cout<< (a | b)<<endl;
+//     return 0;
+// }
+
+//bitwise xor(^) operator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 5, b = 3;
+//     cout<<(a^b)<<endl;
+//     return 0;
+// }
+
+//bitwise not(~) operator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 5;
+//     cout<<(~a)<<endl;
+//     return 0;
+// }
+
+////left shift(<<) operator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 5;
+//     cout<<(a<<1)<<endl;
+//     cout<<(a<<2)<<endl;
+//     return 0;
+// }
+
+
+//right shift(>>) operator
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 5;
+//     cout<<(a>>1)<<endl;
+//     cout<<(a>>2)<<endl;
+//     return 0;
+// }
