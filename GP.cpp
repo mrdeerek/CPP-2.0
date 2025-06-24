b50ec6ae9a4c63421d8a6b854c459cecedec7c3f
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-24 18:57:14 +0530

    geometric progression

---
+++ GP.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a = 2;
+    int r = 2;
+    int n = 4;
+
+    int gp = a * (int)(pow(r,n-1));
+    cout<<gp<<endl;
+
+}
