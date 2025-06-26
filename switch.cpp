c673aeda173a121634c7bf571633c9c7d47f805d
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-26 18:41:00 +0530

    Switch statement

---
+++ switch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day;
+    cin>>day;
+
+    switch(day){
+        case 1: 
+            cout<<"Monday"<<endl;
+            break;
+        
+        case 2:
+            cout<<"Tuesday"<<endl;
+            break;
+
+        case 3:
+            cout<<"Wednesday"<<endl;
+            break;
+
+        case 4:
+            cout<<"Thursday"<<endl;
+            break;
+
+        default:
+            cout<<"Some random day"<<endl;
+    }
+    return 0;
+}
