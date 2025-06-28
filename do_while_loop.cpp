33e52722fa607f514cabcf0f8fea5874902e2e23
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-28 15:00:53 +0530

    do while loop

---
+++ do_while_loop.cpp
@@ -0,0 +1,36 @@
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 3;
+//     do{
+//         cout<<"Kunal"<<endl;
+//         i++;
+//     }
+//     while(i<3);
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char choice;
+//     do{
+//         cout<<"Enter your choice(y/n)"<<endl;
+//         cin>>choice;
+//     }
+//     while(choice != 'n');
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+
+//     do{
+//         cout<<"Enter your number"<<endl;
+//         cin>>num;
+//     }
+//     while(num!=0);
+// }
