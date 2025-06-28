5665c322009d35f4f5d21672548aea6f51d51711
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-28 16:13:07 +0530

    continue in cpp

---
+++ continue_in_cpp.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,x;
+//     cin>>n>>x;
+
+//     for(int i = 1;i<=n;i++){
+//         if(i%x==0){
+//             continue;
+//         }
+//         cout<<i<<endl;
+//     }
+//     return 0;
+// }
+
+
