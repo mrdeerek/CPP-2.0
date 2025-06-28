5820c84c9f247a38c33af894641038f88818c2e6
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-28 11:44:30 +0530

    for loop

---
+++ for_loop.cpp
@@ -0,0 +1,53 @@
+//basic for loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 0;
+//     for(int i = 0;i<5;i++){
+//         cout<<"Kunal"<<endl;
+//     }
+// }
+
+
+//printing variable value
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 0;
+//     for(int i = 0;i<5;i++){
+//         cout<<"Kunal "<< i <<endl;
+//     }
+// }
+
+
+//modifying the start condition
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 3;
+
+//     for(i = 3;i<3;i++){
+//         cout<<"Kunal"<<endl;
+//     }
+// }
+
+
+//Custom increment
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     for(int i = 1;i<=11;i+=2){
+//         cout<<"Kunal"<<endl;
+//     }
+// }
+
+
+//infinite for loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(;;){
+//         cout<<"kunal"<<endl;
+//     }
+// }
