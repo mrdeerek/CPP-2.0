aa8727b1c80556505a12e39b031f703b283d2f4c
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-28 15:28:24 +0530

    break in cpp

---
+++ break_in_c++.cpp
@@ -0,0 +1,34 @@
+//finding the smallest divisor
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     for(int i= 2;i<=n;i++){
+//         if(n%i==0){
+//             cout<<"Smallest divisor: "<<i<<endl;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+
+// use of break in while loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i = 1;
+//     for(int i = 1;i<=5;i++){
+//         if(i==3){
+//             cout<<i<<" "<<endl;
+//             i++;
+//             break;
+//         }
+//         cout<<i<<" "<<endl;
+//     }
+//     return 0;
+// }
+
+
