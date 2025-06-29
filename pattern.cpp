a8c1e8d60f057623c1b84b48a8d9d8ccd1147bdd
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-06-29 23:53:04 +0530

    pattern

--- pattern.cpp
+++ pattern.cpp
@@ -30,17 +30,95 @@
 
 
 //Number grid pattern
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
 
-    for(int i = 1;i<=n;i++){
-        for(int j = 1;j<=n;j++){
-            cout<<j<<" ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
+//     for(int i = 1;i<=n;i++){
+//         for(int j = 1;j<=n;j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// triangle pattern
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     for(int i = 1;i<=n;i++){
+//         for(int j = 1;j<=n;j++){
+//             if(j<=i){
+//                 cout<<"*";
+//             }else{
+//                 " ";
+//             }
+//         }cout<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     for(int i = 1;i<=n;i++){
+//         for(int j = 1;j<=n;j++){
+//             if(j<=i){
+//                 cout<<"* ";
+//             }else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+//Inverted triangle
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     for(int i = 0;i<n;i++){
+//         for(int j = 0;j<i;j++){
+//            cout<<" ";
+//         }
+//         for(int j = 0;j<n-i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+//Square pattern
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     for(int i = 0;i<n;i++){
+//         for(int j= 0;j<n;j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
