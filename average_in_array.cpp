f95653f7db45b1cf5bcaae78e11c498ff76de8ef
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-07-10 12:01:27 +0530

    average

---
+++ average_in_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+double find_average(int arr[], int n){
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum+=arr[i];
+    }
+    return (double)sum/n;
+}
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<< find_average(arr,n);
+    return 0;
+}
