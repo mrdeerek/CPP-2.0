444da899d32c38efa96339c602f1ac0a945f2020
KUNAL RAJ <165830337+mrdeerek@users.noreply.github.com>
2025-08-07 19:32:43 +0530

    Add files via upload

---
+++ source_destination_recursion_example.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void reachHome(int src, int dest){
+    cout<<"Source: "<<src<<", Destination: "<<dest<<endl;
+    if(src == dest){
+        cout<<"Reached home"<<endl;
+        return;
+    }
+   
+    src++;
+
+    reachHome(src, dest);
+}
+int main(){
+    int src = 1;
+    int dest = 10;
+    cout<<endl;
+
+    reachHome(src, dest);
+}
